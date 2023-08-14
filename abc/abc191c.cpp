7b397af1f48814e2f24d6925e55e82fbe479db7a
kogino <kogino@icepp.s.u-tokyo.ac.jp>
2023-08-14 16:53:12 +0900

    20230814

--- abc/abc191c.cpp
+++ abc/abc191c.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main(){
-  int H, W;
-  cin >> H >> W;
-  
-  vector<string> S(H);
-  for(auto &x : S) cin >> x;
-  
-  int ans = 0;
-  for(int i = 0; i < H - 1; i++){
+    int H, W;
+    cin >> H >> W;
+    
+    vector<string> S(H);
+    for(auto &x : S) cin >> x;
+
+    int ans = 0;
+    for(int i = 0; i < H - 1; i++){
     for(int j = 0; j < W - 1; j++){
-      int sum = 0;
-      sum += (S[i][j] == '#');
-      sum += (S[i + 1][j] == '#');
-      sum += (S[i][j + 1] == '#');
-      sum += (S[i + 1][j + 1] == '#');
-      
-      if(sum == 1 || sum == 3) ans++;
+        int sum = 0;
+        sum += (S[i][j] == '#');
+        sum += (S[i + 1][j] == '#');
+        sum += (S[i][j + 1] == '#');
+        sum += (S[i + 1][j + 1] == '#');
+
+        if(sum == 1 || sum == 3) ans++;
     }
-  }
-  
-  cout << ans;
+    }åç
+
+    cout << ans;
 }
